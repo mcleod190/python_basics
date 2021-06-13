6f3dde0793dfe986b3c86aad224c754e8804280f
Dima Maslou <mcleod190@gmail.com>
2021-06-13 17:03:19 +0300

    completed task_3

---
+++ task_3.py
@@ -0,0 +1,10 @@
+#Реализовать склонение слова процент для чисел до 20
+
+for i in range(1,20):
+    if i == 1:
+        ending = ''
+    elif 1 < i < 5:
+        ending = 'а'
+    else:
+        ending = 'ов'
+    print(i, 'процент' + ending)
