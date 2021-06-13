6bf3cabb9b4639875a4ed9905f7ea251970e3c44
Dima Maslou <mcleod190@gmail.com>
2021-06-13 15:44:28 +0300

    completed task_1

---
+++ task_1.py
@@ -0,0 +1,24 @@
+duration = int(input('Enter duration in seconds > '))
+print("Duration = ", duration)
+
+seconds_in_minute = 60
+seconds_in_hour = 3600
+seconds_in_day = 86400
+
+if duration <= seconds_in_minute:
+    print(duration, 'сек')
+elif duration <= seconds_in_hour:
+    seconds = duration % seconds_in_minute
+    minutes = (duration - seconds) // seconds_in_minute
+    print (minutes, 'мин', seconds, 'сек')
+elif seconds_in_hour < duration <= seconds_in_day:
+    seconds = duration % seconds_in_minute
+    hours = (duration - seconds) // seconds_in_hour
+    minutes = (duration - hours * seconds_in_hour )  // seconds_in_minute
+    print (hours, 'час', minutes, 'мин', seconds, 'сек')
+else:
+    seconds = duration % seconds_in_minute
+    days = (duration - seconds) // seconds_in_day
+    hours = (duration - days * seconds_in_day) // seconds_in_hour
+    minutes = (duration - days * seconds_in_day - hours * seconds_in_hour) // seconds_in_minute
+    print (days, 'дн', hours, 'час', minutes, 'мин', seconds, 'сек')   
